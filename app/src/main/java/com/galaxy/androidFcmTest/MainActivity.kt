b281ccd2f9a2af7157702fa380137fd33fa59438
박수빈 <jrkd2165@gmail.com>
2023-10-23 17:53:23 +0900

    :sparkles: feat: fcm 알림 받을 디바이스 토큰 발급 메서드 구현 (수정필요)

--- app/src/main/java/com/galaxy/androidFcmTest/MainActivity.kt
+++ app/src/main/java/com/galaxy/androidFcmTest/MainActivity.kt
@@ -1,68 +1,20 @@
-package com.galaxy.androidFcmTest
-
-import android.annotation.SuppressLint
-import android.content.pm.PackageManager
-import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.content.ContextCompat
-import java.util.jar.Manifest
+import androidx.appcompat.app.AppCompatActivity
+import com.galaxy.androidFcmTest.R
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding : ActivityMainBinding
-
-    @SuppressLint("WrongThread")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        //FCM 설정, 토큰 값 가져오기
-        MyFirebaseMessagingService().getFirebaseToken()
-
-        //PostNotification 대응
-        checkAppPushNotification()
-
-        //DynamicLink 수신 확인
-        initDynamicLink()
+        setContentView(R.layout.activity_main)
     }
 
-    private fun checkAppPushNotification(){
-        //android 13 이상 + 푸시권한 x
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU
-            && PackageManager.PERMISSION_DENIED == ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS)){
-            //푸쉬 권한 x
-            permissionPostNotification.launch(Manifest.permission.POST_NOTIFICATIONS)
-            return
-        }
-
-        //권한 있는 경우
-    }
-
-    //권한 요청
-    private val permissionPostNotification = registerForActivityResult(ActivityResultContracts.RequestPermission()){ isGranted ->
-        if(isGranted){
-            //권한 허용
-        } else {
-            //권한 비허용
+    companion object {
+        // Firebase 디바이스 토큰을 처리할 메서드를 정의
+        fun onTokenRefresh(token: String) {
+            // 토큰을 이용하여 필요한 작업을 수행
+            // 이 메서드를 호출하여 토큰을 MainActivity에서 처리할 수 있음
+            // 예: 토큰을 서버로 보내거나 로컬 저장소에 저장
         }
     }
-
-    //DynamicLink
-    private fun initDynamicLink(){
-        val dynamicLinkData = intent.extras
-        if(dynamicLinkData != null){
-            var dataStr = "DynamicLink 수신받은 값\n"
-            for (key in dynamicLinkData.keySet()){
-                dataStr += "key: $key / value: ${dynamicLinkData.getString(key)}\n"
-            }
-            binding.tvToken.text = dataStr
-        }
-
-    }
-
-
-
 
 }
